247a7ac24dffb5679a68bf8c999691640aca53f6
iskya3 <iskya3@gmail.com>
2026-01-15 05:16:02 -0500

    W

---
+++ addons/main/config.cpp
@@ -0,0 +1,11 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ADDON
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+	};
+};
---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,12 @@
+#define MAINPREFIX 9
+#define PREFIX 9thAux
+
+#include "script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL
+
+
+
+#include "script_macro.hpp"
