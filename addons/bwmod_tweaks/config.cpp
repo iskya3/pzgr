7d3895bf243415e3b899d5128bbbc85f79dd9b7f
iskya3 <iskya3@gmail.com>
2026-01-17 17:19:57 -0500

    Update config.cpp

--- addons/bwmod_tweaks/config.cpp
+++ addons/bwmod_tweaks/config.cpp
@@ -15,6 +15,7 @@ class CfgPatches
 class CfgMagazines 
 {
     class CA_Magazine;
+	class BWA3_1Rnd_Flare_Singlestar_White;
     class BWA3_200Rnd_556x45: CA_Magazine 
 	{
         ACE_isBelt = 1;
@@ -35,6 +36,14 @@ class CfgMagazines
 		ACE_isBelt = 1;
         tracersEvery = 1;
 
+    };
+    class BWA3_1Rnd_Flare_Singlestar_Green: BWA3_1Rnd_Flare_Singlestar_White 
+	{
+        ammo = "Sub_F_Signal_Green";
+    };
+    class BWA3_1Rnd_Flare_Singlestar_Red: BWA3_1Rnd_Flare_Singlestar_White 
+	{
+        ammo = "Sub_F_Signal_Red";
     };
 };
 class CfgAmmo
@@ -76,7 +85,6 @@ class CfgAmmo
 		hit = 11.55;
 	};
 };
-
 class CfgVehicles
 {
 	class TFAR_Bag_Base;
@@ -87,5 +95,20 @@ class CfgVehicles
 	class TFAR_rt1523g_big: TFAR_rt1523g 
 	{
 		maximumLoad = 320;
+	};
+};
+class CfgWeapons
+{
+    class Pistol_Base_F;
+    class BWA3_P2A1: Pistol_Base_F 
+	{
+        scope = 2;
+        magazines[] = 
+		{
+			"BWA3_1Rnd_Flare_Illum",
+            "BWA3_1Rnd_Flare_Singlestar_White",
+            "BWA3_1Rnd_Flare_Singlestar_Green",
+            "BWA3_1Rnd_Flare_Singlestar_Red"
+        };
 	};
 };
