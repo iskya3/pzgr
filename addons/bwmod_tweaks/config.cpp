58624184b17cd81d0cefd04f26ba9b41b5b389a9
iskya3 <iskya3@gmail.com>
2026-01-17 10:43:57 -0500

    Added Ammo tweaks for 30mm HE and fixed pathing on magazine in JPC model

---
+++ addons/bwmod_tweaks/config.cpp
@@ -0,0 +1,91 @@
+class CfgPatches
+{
+	class ADDON
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[]=
+		{
+			"bwa3_weapons",
+			"tfar_backpacks"
+		};
+	};
+};
+class CfgMagazines 
+{
+    class CA_Magazine;
+    class BWA3_200Rnd_556x45: CA_Magazine 
+	{
+        ACE_isBelt = 1;
+        mass = 25;
+    };
+    class BWA3_200Rnd_556x45_Tracer: BWA3_200Rnd_556x45 
+	{
+		ACE_isBelt = 1;
+        tracersEvery = 1;
+    };
+    class BWA3_120Rnd_762x51_soft: CA_Magazine 
+	{
+        ACE_isBelt = 1;
+        mass = 25;
+    };
+    class BWA3_120Rnd_762x51_Tracer_soft: BWA3_120Rnd_762x51_soft 
+	{
+		ACE_isBelt = 1;
+        tracersEvery = 1;
+
+    };
+};
+class CfgAmmo
+{
+    class B_30mm_MP_Tracer_Red;
+	class Sh_120mm_HE_Tracer_Red;
+	class B_556x45_Ball_Tracer_Red;
+	class B_762x51_Tracer_Red;
+    class BWA3_B_30mm_HE: B_30mm_MP_Tracer_Red 
+	{
+		hit = 100; 
+		indirectHit = 10;     
+		indirectHitRange = 3; 
+		caliber = 4.8;      
+		explosive = 0.6;    
+    };
+    class BWA3_Sh_120mm_HE: Sh_120mm_HE_Tracer_Red {
+        hit = 600;
+        indirectHit = 20;
+        indirectHitRange = 8;
+        typicalSpeed = 900;
+        explosive = 0.7;
+    };
+    class BWA3_B_556x45_Ball: B_556x45_Ball_Tracer_Red 
+	{
+		hit=9;
+		typicalSpeed=960;
+		indirectHit =0;
+		indirectHitRange=0;
+		airFriction=-0.000772;
+
+	};
+    class BWA3_B_556x45_Ball_Tracer_Dim: BWA3_B_556x45_Ball 
+	{
+        nvgOnly = 1;
+    };
+    class BWA3_B_762x51_Ball: B_762x51_Tracer_Red 
+	{
+		hit = 11.55;
+	};
+};
+
+class CfgVehicles
+{
+	class TFAR_Bag_Base;
+	class TFAR_rt1523g: TFAR_Bag_Base 
+	{
+		maximumLoad = 200;
+	};
+	class TFAR_rt1523g_big: TFAR_rt1523g 
+	{
+		maximumLoad = 320;
+	};
+};
