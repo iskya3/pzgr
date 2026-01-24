23f1d4a712e91b57c19a4fa5b58707606b563527
iskya3 <iskya3@gmail.com>
2026-01-24 00:14:45 -0500

    New

--- addons/vests/config.cpp
+++ addons/vests/config.cpp
@@ -15,7 +15,7 @@ class cfgWeapons
 {
 	class VestItem;
 	class ItemCore;
-	class generic_jpc_1: ItemCore
+	class jpc_1: ItemCore
 	{
 		author="isky";
 		picture="\pzgr33\pzgr\addons\uniforms\pzgr33_ca.paa";
@@ -31,7 +31,52 @@ class cfgWeapons
 			containerClass="Supply200";
 			modelSides[]={3,1};
 			armor=10;
-			passThrough=0.40000001;
+			passThrough=0.4;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=10;
+					PassThrough=0.40000001;
+				};
+				class Diaphragm
+				{
+					HitpointName="HitDiaphragm";
+					armor=10;
+					PassThrough=0.40000001;
+				};
+				class Abdomen
+				{
+					hitpointName="HitAbdomen";
+					armor=10;
+					PassThrough=0.40000001;
+				};
+				class Body
+				{
+					hitpointName="HitBody";
+					PassThrough=0.40000001;
+				};
+			};
+		};
+	};
+	class jpc_mg_1: ItemCore
+	{
+		author="isky";
+		picture="\pzgr33\pzgr\addons\uniforms\pzgr33_ca.paa";
+		logo="";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="[PzGr33] JPC MG (1)";
+		model="pzgr33\pzgr\addons\vests\jpc_mg.p3d";
+		class ItemInfo: VestItem
+		{
+			mass=25;
+			uniformModel="pzgr33\pzgr\addons\vests\jpc_mg.p3d";
+			containerClass="Supply200";
+			modelSides[]={3,1};
+			armor=10;
+			passThrough=0.4;
 			class HitpointsProtectionInfo
 			{
 				class Chest
